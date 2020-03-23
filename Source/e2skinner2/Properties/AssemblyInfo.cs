84585010f01098963e5b8087706271feb5e1325c
Humaxx <johnnybargeld@gmx.de>
2020-03-23 15:22:37 +0100

    Add files via upload

--- Source/e2skinner2/Properties/AssemblyInfo.cs
+++ Source/e2skinner2/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.0.0")]
-[assembly: AssemblyFileVersion("3.2.0.0")]
+[assembly: AssemblyVersion("3.2.3.0")]
+[assembly: AssemblyFileVersion("3.2.3.0")]
 [assembly: NeutralResourcesLanguageAttribute ("en")]
