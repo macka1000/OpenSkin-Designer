325523ed3bc0f472548de6c01a39884f463fbdc7
Humaxx <johnnybargeld@gmx.de>
2020-03-27 12:08:20 +0100

    Add global var

--- Source/e2skinner2/Structures/sAttribute.cs
+++ Source/e2skinner2/Structures/sAttribute.cs
@@ -20,6 +20,8 @@ namespace OpenSkinDesigner.Structures
         public static bool ShowMsgFallbackFont { get; set; } = true; // Show Messagebox if OSD uses a 'Fallback-Font' (when font not found / declared)
         public static bool ShowMsgFallbackColor { get; set; } = true; // Show Messagebox if OSD uses a 'Fallback-Color' (When missing Color)
         public static bool ShowMsgFontNotFound { get; set; } = true; // Show Messagebox if a Font is not found
+
+        public static bool UseFullAttList { get; set; } = true; // Scintilla - autoComplete, use full Attribut list instead of normal / old one
         public static Color FallbackColor { get; set; } = Color.HotPink; 
         public static List<String> SkinName = new List<String>();
         public static List<String> SkinValue1 = new List<String>();
